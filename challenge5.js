a2ed0ec2cac1f43087db16c979b0347469b31e2c
Shamanth087 <63060562+Shamanth087@users.noreply.github.com>
2020-11-22 22:06:53 +0100

    Create challenge5.js

---
+++ challenge5.js
@@ -0,0 +1,8 @@
+var original="34567";
+var originsplit=original.split("");
+for(var i=0;i<originsplit.length;i++){
+    if(originsplit[i]>5)
+    console.log("1");
+    else
+    console.log("0");
+}
